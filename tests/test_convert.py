8d84da8566b275a8b09beee19564b9e5cea9cc3c
Alloystorm <alloystorm@gmail.com>
2026-02-04 16:07:50 +1100

    add test script

---
+++ tests/test_convert.py
@@ -0,0 +1,11 @@
+import sys
+import os
+import numpy as np
+import shutil
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+
+from vmd2png.vmd import write_vmd, parse_vmd
+from vmd2png.converter import export_vmd_to_files, convert_motion_to_vmd, load_motion_dict
+
+export_vmd_to_files("../data/conqueror_lit.vmd", "test_output/conqueror_lit.png")
