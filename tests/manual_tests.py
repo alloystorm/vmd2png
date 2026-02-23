dd17942632f39faff9a85096db7ab669850377e3
alloystorm <alloystorm@gmail.com>
2026-02-24 00:50:36 +1100

    test

--- tests/manual_tests.py
+++ tests/manual_tests.py
@@ -9,10 +9,10 @@ from vmd2png.vmd import write_vmd, parse_vmd
 from vmd2png.converter import export_vmd_to_files, convert_motion_to_vmd, load_motion_dict
 from vmd2png.preview import preview_motion
 
-#export_vmd_to_files("data/conqueror.vmd", "test_output", leg_ik=False, camera_vmd_path="data/conqueror_cam.vmd")
+export_vmd_to_files("data/conqueror.vmd", "test_output", leg_ik=False, camera_vmd_path="data/conqueror_cam.vmd")
 #convert_motion_to_vmd("test_output/conqueror.png", "test_output/reconstructed.vmd")
 #preview_motion("test_output/reconstructed.vmd", leg_ik=False)
-preview_motion("test_output/conqueror_3.png", leg_ik=False)
+#preview_motion("test_output/conqueror_3.png", leg_ik=False)
 # preview_motion("data/conqueror.vmd", camera_vmd_path="data/conqueror_cam.vmd", leg_ik=False)
 # preview_motion("data/kimagure.vmd", leg_ik=True)
 # preview_motion("data/conqueror.vmd", camera_vmd_path="data/conqueror_cam.vmd")
